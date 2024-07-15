376cadfc89f7569874ed8fa09eaf6ec728a25952
Vishal Yadav <144472297+vishalyadav2000@users.noreply.github.com>
2024-07-15 15:50:14 +0530

    score.js

--- score.js
+++ score.js
@@ -1,1 +1,9 @@
 // Iteration 8: Making scoreboard functional
+const scoreboard=document.getElementById("score-board");
+scoreboard.innerHTML=localStorage.getItem('total')
+
+const playagainbutton=document.getElementById("play-again-button")
+playagainbutton.onclick=function(){
+    // window.location.href="index.html";
+    window.location.href="index.html";
+}
